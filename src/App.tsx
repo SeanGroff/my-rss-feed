d3610b1f578c7ed8c6e42e5aa2803eaf74fca014
Sean Groff <sgroff04@gmail.com>
2020-05-02 15:47:47 -0500

    remove dead code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 
-import { useRSS } from './hooks'
-
-const rssUrls = ['https://www.seangroff.dev/rss.xml']
-
 function App() {
-  const rssFeed = useRSS({ rssUrls })
-  console.log(rssFeed)
   return <div>Hello, world!</div>
 }
 
